5221373cc78002ba789d114b7aeddc380536d898
Marin Masic <mamasic@gmail.com>
2018-11-08 16:24:22 +0100

    - ponavljanje druge lekcije

--- l2/my_try.R
+++ l2/my_try.R
@@ -1,54 +1,38 @@
+data.file <- file.path('01_heights_weights_genders.csv')
+heights.weights <- read.csv(data.file, header = TRUE, sep = ',')
+heights <- with(heights.weights, Height)
+summary(heights)
+
 my.mean <- function(x) {
   return(sum(x) / length(x))
 }
 
-data<-c(1,10,100,1000,10000)
-meanRes<-my.mean(data)
-print(meanRes)
-
 my.median <- function(x) {
-  sorted.x <- sort(x)
+  sorted.x = sort(x)
 
   if (length(x) %% 2 == 0) {
-    indices <- c(length(x) / 2, lentgth(x) / 2 + 1)
-    return(my.mean(sorted.x[indices]))
+    # kako ne postoji vrijednost koja je točno u sredini vraćam
+    # aritmetičku sredinu dvije središnje vrijednosti
+    indices <- c(length(x) / 2, length(x) / 2 + 1) 
+    return(mean(sorted.x[indices]))
   } else {
     index <- ceiling(length(x) / 2)
     return(sorted.x[index])
   }
 }
 
-medRes = my.median(data)
-print(medRes)
+my.mean(heights)
+mean(heights)
+my.median(heights)
+median(heights)
 
-print(min(data))
-print(max(data))
-print(range(data))
-print(quantile(data))
-print(quantile(data, probs = seq(0, 1, by = .2)))
-print(quantile(data, probs = .33))
+# par najveće i najmanje vrijednosti
+c(min(heights), max(heights))
+range(heights)
 
-my.var <- function(x) {
-  return ((sum((x - my.mean(x)) ^ 2)) / (length(x) - 1))
-}
+# unaprijed zadane vrijednosti kvantila
+quantile(heights)
+# postavljanje vrijednosti kvantila
+quantile(heights, probs = seq(0, 1, by = .2))
 
-print(my.var(data))
-print(var(data))
-
-my.sd <- function(x) {
-  return(sqrt(my.var(x)))
-}
-
-print(my.sd(data))
-print(sd(data))
-
-# crtkaranje
-library('ggplot2')
-heights.file <- file.path('01_heights_weights_genders.csv')
-heights.weights <- read.csv(heights.file, header = TRUE, sep = ',')
-#ggplot(heights.weights, aes(x = Height)) + geom_histogram(binwidth = 1)
-#ggplot(heights.weights, aes(x = Height)) + geom_histogram(binwidth = 0.001)
-#ggplot(heights.weights, aes(x = Height)) + geom_density()
-#ggplot(heights.weights, aes(x = Height, fill = Gender)) + geom_density()
-#ggplot(heights.weights, aes(x = Weight, fill = Gender)) + geom_density()
-ggplot(heights.weights, aes(x = Weight, fill = Gender)) + geom_density() + facet_grid(Gender ~ .)
+# 41. stranica
