016b46b7ce888325f8652873f922e7bbcb04eef3
Marin Masic <mamasic@gmail.com>
2019-01-16 11:09:56 +0100

    - extra komentari

--- l3/my_try.R
+++ l3/my_try.R
@@ -16,8 +16,10 @@ get.msg <- function(path) {
   # seq parametri - od, do, za koliko se povećava
   # which - nađi item u nizu koji je prazan string i vrati njegov indeks uvećan za jedan
   # sve skupa - vrati članove niza od prvog praznog reda pa do kraja
+  # prvi prazan red odvaja zaglavlje o tijela poruke
   msg <- text[seq(which(text=="")[1]+1, length(text), 1)]
   close(con)
+  # paste konkatenira stringove
   return (paste(msg, collapse="\n"))
 }
 
@@ -25,9 +27,35 @@ spam.docs <- dir(spam.path) # lista imena datoteka
 spam.docs <- spam.docs[which(spam.docs != "cmds")] # izbaci članove koji su jednaki "cmds"
 all.spam <- sapply(spam.docs, function(p) get.msg(paste(spam.path, p, sep="")))
 
-#print(head(all.spam.))
+#print(head(all.spam))
 
 get.tdm <- function(doc.vec) {
   doc.corpus <- Corpus(VectorSource(doc.vec))
-  # stranica 82
+  control <- list(stopwords=TRUE, removePunctuation=TRUE, removeNumbers=TRUE, minDocFreq=2)
+  doc.dtm <- TermDocumentMatrix(doc.corpus, control)
+  return (doc.dtm)
 }
+
+spam.tdm <- get.tdm(all.spam)
+
+spam.matrix <- as.matrix(spam.tdm)
+spam.counts <- rowSums(spam.matrix)
+spam.df <- data.frame(cbind(names(spam.counts), as.numeric(spam.counts)), stringsAsFactors=FALSE)
+names(spam.df) <- c("term", "frequency")
+spam.df$frequency <- as.numeric(spam.df$frequency)
+
+spam.occurrence <- sapply(1:nrow(spam.matrix), function (i) { length(which(spam.matrix[i,] > 0))/ncol(spam.matrix)})
+spam.density <- spam.df$frequency/sum(spam.df$frequency)
+
+spam.df <- transform(spam.df, density=spam.density, occurrence=spam.occurrence)
+
+print(head(spam.df))
+#n = c(2, 3, 5) 
+#s = c("aa", "bb", "cc") 
+#b = c(TRUE, FALSE, TRUE) 
+#df = data.frame(n, s, b)   
+#print(df)
+
+#print(spam.df$frequency/sum(spam.df$frequency))
+
+print(paste("jedan", "dva", sep="+", collapse="-"))
